d6e6d054061cb9bfca682ab29d01a6e6a9281573
root <root@localhost.localdomain>
2017-10-10 22:42:26 -0500

    Post shows

--- ThaliaTix/thalia.py
+++ ThaliaTix/thalia.py
@@ -1,13 +1,40 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
+#Replace by "database", maybe a conf file
+shows = []
+#Replace by reading a conf file
+wid = 0
+sid = 0
+mrid = 0
+oid = 0
+
+def checkData(payload, expected):
+    if expected == 'POSTshow':
+        data = json.loads(payload)
+        if not (len(data) == 2)|(len(data["show_info"]) == 4):
+            return False
+        for each in data["seating_info"]:
+            if not len(each) == 2:
+                return False
+        return True
+        #Rest of the checking will be regex matching
 
 ######### SHOWS #########
 @app.route('/shows', methods=['GET', 'POST'])
 def viewOrCreateShow():
+    global wid
     if request.method == 'GET':
-
+        return shows
     else:
+        payload = request.get_json(force=True)
+        if checkData(payload, 'POSTshow'):
+            wid +=1
+            dictpayload = json.loads(payload)
+            dictpayload["wid"] = str(wid)
+            shows.append(dictpayload)
+            return json.dumps({'wid': str(wid)})
 
 
 @app.route('/shows/<wid>', methods=['GET', 'PUT', 'DELETE'])
